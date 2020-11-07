44efac46c7354ca592db639adbca6c13e2f4240b
Lukas <ll8455@gmail.com>
2020-11-07 19:35:10 +0200

    5 uzdutis

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "pages#show", page: "home"
+  
+  match '/home', to: 'pages#home', via: 'get'
+  match '/cat', to: 'another#cat', via: 'get'
+  match '/dog', to: 'pages#dog', via: 'get'
+  match '/about', to: 'pages#about', via: 'get'
 end
